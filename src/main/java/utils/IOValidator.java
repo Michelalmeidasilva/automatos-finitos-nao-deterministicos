f953d9052f32559b815c85b37865c2a2cf734f41
Michel Silva <michelalmeidasilva@outlook.pt>
2020-10-04 17:56:19 -0300

    metodos para converter matriz char em matriz int e vetor char para int

--- src/main/java/utils/IOValidator.java
+++ src/main/java/utils/IOValidator.java
@@ -3,7 +3,12 @@ package main.java.utils;
 import main.java.exceptions.InvalidFormatException;
 
 public class IOValidator {
-  public static boolean input(String fitaDeEntrada, String[] alfabeto) throws InvalidFormatException {
+  private  boolean debug;
+  private int linha ;
+  private int coluna = 0;
+  private int  profundidade = 0 ;
+
+  public  static boolean input(String fitaDeEntrada, String[] alfabeto) throws InvalidFormatException {
     boolean validar = false;
     for (int i = 0; i < fitaDeEntrada.length(); i ++) {
       validar = false;
@@ -16,5 +21,49 @@ public class IOValidator {
       if(validar == false) throw new InvalidFormatException("Alfabeto não esta coerente com a Entrada ['" + fitaDeEntrada + "']");
     }
     return validar;
+  }
+
+  public  int [][][] convertMatriz3DCharToInt(char[][][] matriz){
+    sizeOfMatriz3D(matriz);
+    int [][][] tabelaTransicaoInt  = new int[linha][coluna][profundidade];
+    for (int i = 0; i < matriz.length; i ++){
+      for (int j = 0 ; j < matriz[i].length; j ++){
+        for (int k = 0; k < matriz[i][j].length ; k ++){
+          tabelaTransicaoInt[i][j][k] = Integer.parseInt( matriz[i][j][k] + "");
+          if(debug) System.out.println(tabelaTransicaoInt[i][j][k]);
+        }
+      }
+    }
+    return tabelaTransicaoInt;
+  }
+
+  private  void sizeOfMatriz3D(char [][][] matriz) {
+    this.linha = matriz.length  ;
+    this.coluna = 0;
+    this.profundidade = 0 ;
+    for (int i = 0; i < matriz.length; i ++){
+      this.coluna = matriz[i].length > coluna ? matriz[i].length : coluna;
+      for (int j = 0 ; j < matriz[i].length; j ++){
+        this.profundidade = matriz[i][j].length > profundidade ? matriz[i][j].length : profundidade;
+      }
+    }
+    if(debug){
+      System.out.println("Tamanho max de linha        " + linha);
+      System.out.println("Tamanho max de colunas      " + coluna);
+      System.out.println("Tamanho max de profundidade " + profundidade);
+    }
+  }
+
+  public  int [][][] convertMatriz3DCharToInt(char[][][] matriz,boolean debug) {
+    this.debug = debug;
+    return  convertMatriz3DCharToInt(matriz);
+  }
+
+  public  int [] convertArrayCharToArrayInt(char[] array) {
+    int [] arrayInt = new int[array.length];
+    for (int i = 0; i < array.length; i++) {
+      arrayInt[i] = Integer.parseInt( array[i] + "");;
+    }
+    return arrayInt;
   }
 }
