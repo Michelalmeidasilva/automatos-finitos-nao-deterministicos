5344b428658c036b29e73c42b66418e47e1a544f
Michel Silva <michelalmeidasilva@outlook.pt>
2020-10-04 02:48:50 -0300

    refatoracao do codigo

--- src/main/java/domain/NaoDeterministicoParalelo.java
+++ src/main/java/domain/NaoDeterministicoParalelo.java
@@ -4,79 +4,77 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class NaoDeterministicoParalelo {
-  protected String  fitaDeEntrada;               // Σ é um alfabeto (de símbolos) de entrada
-  protected int estadoInicial ;                  //  q0 é um elemento distinguido de Q: estado inicial
-  int [][][] transicao  ;
-  int aceitacao[];
+  private String  fitaDeEntrada;               //  Σ é um alfabeto (de símbolos) de entrada
+  private int estadoInicial ;                  //  q0 é um elemento distinguido de Q: estado inicial
+  private int aceitacao[];
+  private int [][][] transicao  ;
+
+  public NaoDeterministicoParalelo(int [][][] transicao, int[] estadosAceitacao, int estadoInicial ){
+    this.aceitacao = estadosAceitacao;
+    this.estadoInicial = estadoInicial  ;
+    this.transicao = transicao;
+  };
 
   public NaoDeterministicoParalelo(int [][][] transicao, int[] estadosAceitacao ){
     this.aceitacao = estadosAceitacao;
-    this.estadoInicial = 0;
+    this.estadoInicial = 0 ;
     this.transicao = transicao;
   };
 
   public boolean execucao(String fitaDeEntrada){
+    int posicao =0;
     this.fitaDeEntrada = fitaDeEntrada;
-    return controleFinito();
+    return controleFinito(posicao);
   }
 
-  private boolean controleFinito(){
-    String entrada = fitaDeEntrada;
-    int posicao =0 ;
-    int [] estados ={estadoInicial};
-    while(posicao < entrada.length()){
-      imprimeCI(entrada, estados, posicao);
-      int elemento = Integer.parseInt(entrada.substring(posicao, posicao +1 ));
+  private boolean controleFinito(int posicao){
+    int [] estados = {estadoInicial};
+    while(posicao < fitaDeEntrada.length()){
+      imprimeCI(fitaDeEntrada, estados, posicao);
+      int elemento = Integer.parseInt(fitaDeEntrada.substring(posicao, posicao +1 ));
       int[] novosEstados = new int[]{};
+
       for (int i: estados){
-        //retorna um conjunto de possiveis transições
         int [] destinoTransicao = transicao[i][elemento];
         novosEstados = uniao(novosEstados, destinoTransicao);
         estados = novosEstados;
       }
-      if(estados.length == 0 ){
+
+      if(estados.length == 0 )
         break;
-      }
+
       posicao ++;
     }
-    imprimeCI(entrada, estados, posicao);
+    imprimeCI(fitaDeEntrada, estados, posicao);
 
-    if(aceita(estados))
+    if(verificaAceitacao(estados))
       return true;
     else
       return false;
-
   }
 
   private int[] uniao(int[] estados, int[] novosEstados) {
     Set<Integer> uniao = new TreeSet<>();
-    for (int i: estados){
-      uniao.add(i);
-    }
-    for(int i: novosEstados){
-      uniao.add(i);
-    }
+
+    for (int i: estados)    uniao.add(i);
+    for(int i: novosEstados)   uniao.add(i);
+
     int[] ret = new int[uniao.size()];
     int j =0 ;
     for(int i: uniao) ret[j++]   = i;
     return ret;
   }
 
-
   private void imprimeCI(String entrada,int []estado,  int posicao) {
-    System.out.print(entrada.substring(0, posicao) );
-    System.out.print("[" );
+    System.out.print(entrada.substring(0, posicao) +  "[");
     for (int i = 0; i<  estado.length; i ++){
       System.out.print("q"+ estado[i] );
-      if (i < estado.length - 1) {
-        System.out.print(",");
-      }
+      if (i < estado.length - 1)  System.out.print(",");
     }
-    System.out.print("]");
-    System.out.println(entrada.substring(posicao));
+    System.out.println("]" + entrada.substring(posicao));
   }
 
-  private boolean aceita(int[] estados) {
+  private boolean verificaAceitacao(int[] estados) {
     if(estados == null) return false;
     for (int i : estados){
       for(int j : aceitacao){
@@ -85,7 +83,7 @@ public class NaoDeterministicoParalelo {
         }
       }
     }
-    return false ;
+    return false;
   }
 
   public int[][] convertToDFA(){
