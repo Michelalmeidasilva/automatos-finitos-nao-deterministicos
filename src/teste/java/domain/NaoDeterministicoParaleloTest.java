b82d6d43802156d8f8224112bef33e308166e69a
Michel Silva <michelalmeidasilva@outlook.pt>
2020-10-04 17:54:07 -0300

    testes para verificar automatos nao deterministicos adicionados

--- src/teste/java/domain/NaoDeterministicoParaleloTest.java
+++ src/teste/java/domain/NaoDeterministicoParaleloTest.java
@@ -2,6 +2,7 @@ package teste.java.domain;
 
 import main.java.domain.NaoDeterministicoParalelo;
 import main.java.exceptions.IsNotBelongOnLanguage;
+import main.java.utils.IOValidator;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -21,12 +22,33 @@ public class NaoDeterministicoParaleloTest {
     int estadoInicial = 0;
     String  fitaDeEntrada = "010";
     NaoDeterministicoParalelo afnd = new NaoDeterministicoParalelo(tabelaTransicao, estadosAceitacao,estadoInicial);
-      assertEquals("Deve retornar false para  ser uma entrada invalida, baseada no automato", false,  afnd.execucao(fitaDeEntrada, true));
+      assertEquals("Deve retornar false para  ser uma palavra invalida, ou seja, não pertence ao automato", false,  afnd.execucao(fitaDeEntrada, true));
   }
+
+
+
+
+  @Test
+  /// Automato base https://ibb.co/dKMfc1H
+  public void shouldBeConvertUseChar()   {
+    char [][][]  tabelaTransicao ={
+      //alf|estados         0     1      2   3   4  5   6   7  8  9  10
+      /*q0*/               {{'0','1'},{'0','2'}},
+      /*q1*/               {{'3'}, {}},
+      /*q2*/               {{}, {'3'}},
+      /*q3*/               {{'3'}, {'3'}}};
+    char estadosAceitacao[] = {'0', '1'};
+    char estadoInicial  =0;
+    String  fitaDeEntrada = "0001a";
+    NaoDeterministicoParalelo afnd = new NaoDeterministicoParalelo(tabelaTransicao, estadosAceitacao, estadoInicial);
+    assertEquals("Deve retornar true para  ser uma palavra valida, logo pertence ao automato", true,  afnd.execucao(fitaDeEntrada, true));
+
+  }
+
 
   @Test
   /// Automato base https://ibb.co/zxRc4q3
-  public void shouldBeAcceptWord()   {
+  public void   shouldBeAcceptWord()   {
     int [][][]  tabelaTransicao ={
       //alf|estados         0     1
       /*q0*/             {{0,1},{0,2}},
@@ -35,9 +57,9 @@ public class NaoDeterministicoParaleloTest {
       /*q3*/              {{3}, {3}}};
     int estadosAceitacao[] = {3};
     int estadoInicial = 0;
-    String  fitaDeEntrada = "011";
+    String  fitaDeEntrada = "000";
     NaoDeterministicoParalelo afnd = new NaoDeterministicoParalelo(tabelaTransicao, estadosAceitacao, estadoInicial);
-    assertEquals("Deve retornar true para  ser uma entrada valida, baseada no automato", true,  afnd.execucao(fitaDeEntrada, true));
+    assertEquals("Deve retornar false para  ser uma palavra invalida, logo pertence ao automato", true,  afnd.execucao(fitaDeEntrada, true));
   }
 }
 
